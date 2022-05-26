e479868f21bfb2ccee2ca5453f2903053c2e41be
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-05-26 15:48:43 -0300

    Correções Color e Quantity

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+var data = 0;
+  
+//printing default value of data that is 0 in h2 tag
+document.getElementById('counter').innerText = data;
+  
+//creation of increment function
+function increment() {
+    data = data + 1;
+    document.getElementById('counter').innerText = data;
+}
+//creation of decrement function
+function decrement() {
+    data = data - 1;
+    document.getElementById('counter').innerText = data;
+}

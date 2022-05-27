ded3e4f94045032a96df96b1860c1f5a387b310f
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-05-27 10:30:26 -0300

    Atualização cores

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 'use strict';
 
 var data = 0;
+var price = 54.99;
   
 //printing default value of data that is 0 in h2 tag
 document.getElementById('counter').innerText = data;
+document.getElementById('price').innerText = price;
   
 //creation of increment function
 function increment() {
     data = data + 1;
     document.getElementById('counter').innerText = data;
+    document.getElementById('totalprice').innerText = (data * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});    
 }
 
 //creation of decrement function
 function decrement() {
     data = Math.max(0,data - 1);
     document.getElementById('counter').innerText = data;
+    document.getElementById('totalprice').innerText = (data * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
 }
+
+//toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})

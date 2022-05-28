94518bbb277d2daaf6d3fcdae9902dca0043291a
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-05-28 13:29:29 -0300

    Add. JS to colors
    Adicionado JavaScript para a seleção das cores (ainda não funciona)

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ function decrement() {
     document.getElementById('counter').innerText = data;
     document.getElementById('totalprice').innerText = (data * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
 }
+
+//creation of changeImage function
+let img = document.querySelector('img');
+let btn1 = document.getElementById('#btn1');
+let btn2 = document.getElementById('#btn2');
+let btn3 = document.getElementById('#btn3');
+let btn4 = document.getElementById('#btn4');
+let btn5 = document.getElementById('#btn5');
+
+btn1.addEventListener('click', () => {
+    img.src = 'imagens/1-midnightblack.png'
+})
+
+btn2.addEventListener('click', () => {
+    img.src = 'imagens/2-crimsonred.png'
+})
+
+btn3.addEventListener('click', () => {
+    img.src = 'imagens/3-galaticpurple.png'
+})
+
+btn4.addEventListener('click', () => {
+    img.src = 'imagens/4-starlight blue.png'
+})
+
+btn5.addEventListener('click', () => {
+    img.src = 'imagens/5-novapink.png'
+})
+

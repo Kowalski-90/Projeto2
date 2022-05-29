03ab57176bec878e2f6a308e571de760b7f1d57a
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-05-28 21:21:48 -0300

    Alteração do incremento / decremento

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 'use strict';
 
-var data = 0;
 var price = 54.99;
   
-//printing default value of data that is 0 in h2 tag
-document.getElementById('counter').innerText = data;
+//printing euro html symbol on total price
 document.getElementById('price').innerHTML = price.toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});;
-  
+
+//creation of input function
+let addBtn = document.querySelector('#increment');
+let subBtn = document.querySelector('#decrement');
+let counter = document.querySelector('#counter')
+
+addBtn.addEventListener('click',()=>{
+    counter.value = parseInt(counter.value) + 1
+    document.getElementById('counter').innerText = counter.value;
+    document.getElementById('totalprice').innerText = (counter.value * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
+});
+
+subBtn.addEventListener('click',()=>{
+    counter.value = Math.max(0, counter.value - 1)
+    document.getElementById('counter').innerText = counter.value;
+    document.getElementById('totalprice').innerText = (counter.value * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
+});
+
 //creation of increment function
-function increment() {
+/*function increment() {
     data = data + 1;
     document.getElementById('counter').innerText = data;
     document.getElementById('totalprice').innerText = (data * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});    
@@ -19,7 +34,7 @@ function decrement() {
     data = Math.max(0,data - 1);
     document.getElementById('counter').innerText = data;
     document.getElementById('totalprice').innerText = (data * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
-}
+}*/
 
 //creation of changeImage function
 let img = document.querySelector('img');
@@ -31,21 +46,21 @@ let btn5 = document.getElementById('#btn5');
 
 btn1.addEventListener('click', () => {
     img.src = 'imagens/1-midnightblack.png'
-})
+});
 
 btn2.addEventListener('click', () => {
     img.src = 'imagens/2-crimsonred.png'
-})
+});
 
 btn3.addEventListener('click', () => {
     img.src = 'imagens/3-galaticpurple.png'
-})
+});
 
 btn4.addEventListener('click', () => {
     img.src = 'imagens/4-starlight blue.png'
-})
+});
 
 btn5.addEventListener('click', () => {
     img.src = 'imagens/5-novapink.png'
-})
+});
 

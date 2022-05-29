d4c6b254857dea3e1b1f06b28787666e9b0ac1a7
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-05-28 22:20:21 -0300

    Tentativa alterar cores (failed)

--- script.js
+++ script.js
@@ -22,7 +22,41 @@ subBtn.addEventListener('click',()=>{
     document.getElementById('totalprice').innerText = (counter.value * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
 });
 
-//creation of increment function
+function changeImage() {
+    if(document.getElementById('btn2').clicked == true){
+        alert("Button was clicked");
+    }
+}
+
+//creation of changeImage function
+/*let img = document.querySelector('img');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let btn5 = document.querySelector('#btn5');
+
+btn1.addEventListener('click',()=>{
+    img.src = 'imagens/1-midnightblack.png';
+});
+
+btn2.addEventListener('click',()=>{
+    img.src = 'imagens/2-crimsonred.png';
+});
+
+btn3.addEventListener('click',()=>{
+    img.src = 'imagens/3-galaticpurple.png';
+});
+
+btn4.addEventListener('click',()=>{
+    img.src = 'imagens/4-starlight blue.png';
+});
+
+btn5.addEventListener('click',()=>{
+    img.src = 'imagens/5-novapink.png';
+});*/
+
+//creation of increment function (deprecated)
 /*function increment() {
     data = data + 1;
     document.getElementById('counter').innerText = data;
@@ -35,32 +69,3 @@ function decrement() {
     document.getElementById('counter').innerText = data;
     document.getElementById('totalprice').innerText = (data * price).toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
 }*/
-
-//creation of changeImage function
-let img = document.querySelector('img');
-let btn1 = document.getElementById('#btn1');
-let btn2 = document.getElementById('#btn2');
-let btn3 = document.getElementById('#btn3');
-let btn4 = document.getElementById('#btn4');
-let btn5 = document.getElementById('#btn5');
-
-btn1.addEventListener('click', () => {
-    img.src = 'imagens/1-midnightblack.png'
-});
-
-btn2.addEventListener('click', () => {
-    img.src = 'imagens/2-crimsonred.png'
-});
-
-btn3.addEventListener('click', () => {
-    img.src = 'imagens/3-galaticpurple.png'
-});
-
-btn4.addEventListener('click', () => {
-    img.src = 'imagens/4-starlight blue.png'
-});
-
-btn5.addEventListener('click', () => {
-    img.src = 'imagens/5-novapink.png'
-});
-
